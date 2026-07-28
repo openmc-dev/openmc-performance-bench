6d912d1e0d4d1569463d8b14377f00030beaffe3
Amanda Lund <alund@anl.gov>
2026-07-28 12:53:45 -0500

    Add (4, None) threads, MPI procs config

--- benchmarks/config.py
+++ benchmarks/config.py
@@ -34,6 +34,7 @@ _MPI_ENABLED = _detect_mpi_enabled()
 # Explicit list of (threads, MPI procs) configurations to benchmark
 _CONFIGS: Tuple[Tuple[int, Optional[int]], ...] = (
     (1, None),
+    (4, None),
     (12, None),
     (48, None),
 )
